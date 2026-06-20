c369d33af0535a67f0a927f6366b2aa4bbd9507d
Abdul_Basit_Sanaullah <ab9919353@gmail.com>
2026-06-20 10:24:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,10 @@
 import streamlit as st
-from pypdf import PdfReader
-from transformers import (
-    pipeline,
-    AutoTokenizer,
-    AutoModelForQuestionAnswering
-)
+import transformers
 
-st.title("📚 Document Question Answering Chatbot")
+st.write("Transformers Version:")
+st.write(transformers.__version__)
 
-uploaded_file = st.file_uploader(
-    "Upload PDF",
-    type=["pdf"]
-)
+from transformers.pipelines import SUPPORTED_TASKS
 
-if uploaded_file:
-
-    reader = PdfReader(uploaded_file)
-
-    text = ""
-
-    for page in reader.pages:
-        extracted = page.extract_text()
-
-        if extracted:
-            text += extracted
-
-    st.success("PDF Loaded Successfully!")
-
-    tokenizer = AutoTokenizer.from_pretrained(
-        "deepset/roberta-base-squad2"
-    )
-
-    model = AutoModelForQuestionAnswering.from_pretrained(
-        "deepset/roberta-base-squad2"
-    )
-
-    qa_pipeline = pipeline(
-        task="question-answering",
-        model=model,
-        tokenizer=tokenizer
-    )
-
-    question = st.text_input(
-        "Ask a Question"
-    )
-
-    if st.button("Get Answer"):
-
-        result = qa_pipeline(
-            question=question,
-            context=text[:3000]
-        )
-
-        st.subheader("Answer")
-        st.write(result["answer"])
-
-        st.subheader("Confidence")
-        st.write(f"{result['score']*100:.2f}%")
+st.write("Supported Tasks:")
+st.write(list(SUPPORTED_TASKS.keys()))
